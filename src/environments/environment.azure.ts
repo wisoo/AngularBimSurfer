e975ec14c3eef8322e8f8b1f73dea7132e053e94
JanoSlivka <js.slivka@gmail.com>
2019-03-26 17:22:34 +0100

    Zmena url na bim server.

--- src/environments/environment.azure.ts
+++ src/environments/environment.azure.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://kros-titan-bim.westeurope.cloudapp.azure.com:5500/bimserverwar-1.5.125',
+  apiUrl: 'http://bim-server.westeurope.cloudapp.azure.com:5500/bimservertest',
   login: 'kros@kros.sk',
   password: 'Titan123456789',
 };
