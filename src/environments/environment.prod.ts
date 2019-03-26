f0859051da8b414c5a18fd60f82e8de5945820ce
JanoSlivka <js.slivka@gmail.com>
2019-03-26 17:23:19 +0100

    Nastavanie produk4n0ho prostredia.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://bim-server.westeurope.cloudapp.azure.com:5500/bimservertest',
+  login: 'kros@kros.sk',
+  password: 'Titan123456789',
 };
