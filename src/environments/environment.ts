b41f123805e819c3c677de9907e26cbef94b7836
JanoSlivka <js.slivka@gmail.com>
2019-04-24 18:05:38 +0200

    Fix vulnerabilities.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:8125',
-  login: 'slivka@kros.sk',
-  password: '21kros12',
+    production: false,
+    apiUrl: 'http://bim-server.westeurope.cloudapp.azure.com:5500/bimservertest',
+    login: 'kros@kros.sk',
+    password: 'Titan123456789',
 };
