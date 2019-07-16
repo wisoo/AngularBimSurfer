033371fe52733437a502a7ae20088ac543962c74
JanoSlivka <js.slivka@gmail.com>
2019-07-16 21:17:55 +0200

    Aktualizácia na novší surfer (0.0.247) a server (1.5.155).

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://bim-server.westeurope.cloudapp.azure.com:5500/bimserverdevelopment-1.5.147',
+  apiUrl: 'http://ssw-dev-bim-vm.westeurope.cloudapp.azure.com:8082/bimservertest-1.5.155',
   login: 'kros@kros.sk',
   password: 'Titan123456789',
 };
