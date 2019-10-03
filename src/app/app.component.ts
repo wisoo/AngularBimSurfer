fa9f3ad9610767950049afc8ee6f6f88e502a70a
Wacim <wacim.yassine@syscobat.com>
2019-10-03 10:34:53 +0200

    added the minimum necessary to show a project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { BimServerClient } from 'bimserverapi/bimserverclient';
+import { BimServerViewer } from '@micaw/bimsurfer3/viewer/bimserverviewer';
+import { Settings } from '@micaw/bimsurfer3/viewer/settings';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'fromScratch';
+  settingsView: Settings;
+  settingsElement: HTMLElement;
+  settings: {drawTileBorders: boolean};
+  bimServerViewer: BimServerViewer;
+  ngOnInit(): void {
+    this.settingsElement = document.createElement('div');
+    this.settingsView = new Settings(this.settingsElement);
+    this.settings = JSON.parse(JSON.stringify(this.settingsView.settings));
+    console.log(this.settings);
+    this.settings.drawTileBorders = false;
+    let api = new BimServerClient('http://46.105.124.137:8080/bimserver');
+    api.init(() => {
+      console.log(api);
+      api.login('wacim.yassine@syscobat.com', 'admin', () => {
+        // So now authentication has succeeded, make sure that for a real implementation you also check for errors
+        api.call('ServiceInterface', 'getProjectByPoid', {poid: '131073'}, (project) => {
+          let canvas = document.getElementById('glcanvas');
+          console.log(canvas);
+          console.log(project);
+          this.bimServerViewer = new BimServerViewer(this.settings, canvas, null, null, null);
+          this.bimServerViewer.loadModel(api, project);
+        }, (error) => {
+          console.log('error in call', error);
+        }); },
+        () => {console.log('authentication failed'); });
+    });
+  }
 }
